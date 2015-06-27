b595d74289983b9dc4de8da2cb8e472cff3af32a
Dan Cooper <dcooper@saymedia.com>
2015-06-27 12:42:09 -0700

    use spaces

--- primes.py
+++ primes.py
@@ -1,10 +1,10 @@
 def IsPrime( Number ):
-	divisor = 3
-   	while divisor <= Number/2:
-   		if Number % divisor == 0:
-   			return False
-   		divisor = divisor + 1
-   	return True
+    divisor = 3
+    while divisor <= Number/2:
+        if Number % divisor == 0:
+            return False
+        divisor = divisor + 1
+    return True
 
 NumberPrimes = 3
 MaxPrimes = 100
@@ -12,8 +12,8 @@ CurrentNumber = 3
 print "(1): 1"
 print "(2): 2"
 while NumberPrimes <= MaxPrimes:
-	if IsPrime(CurrentNumber):
-		print "(", NumberPrimes, "): ", CurrentNumber
-		NumberPrimes = NumberPrimes + 1
-	CurrentNumber = CurrentNumber + 2
+    if IsPrime(CurrentNumber):
+        print "(", NumberPrimes, "): ", CurrentNumber
+        NumberPrimes = NumberPrimes + 1
+    CurrentNumber = CurrentNumber + 2
 
